d4b23941515fa0b0472be60e0baf8f64cf0f61b3
Kevin Kreiser <kevinkreiser@gmail.com>
2019-02-08 10:37:45 -0500

    gcc says these aren't found in the global scope

--- include/util/ieee754.hpp
+++ include/util/ieee754.hpp
@@ -247,8 +247,9 @@ inline DiyFp GetCachedPower(int e, int *K)
 inline void
 GrisuRound(char *buffer, int len, uint64_t delta, uint64_t rest, uint64_t ten_kappa, uint64_t wp_w)
 {
-    while (rest < wp_w && delta - rest >= ten_kappa && (rest + ten_kappa < wp_w || /// closer
-                                                        wp_w - rest > rest + ten_kappa - wp_w))
+    while (rest < wp_w && delta - rest >= ten_kappa &&
+           (rest + ten_kappa < wp_w || /// closer
+            wp_w - rest > rest + ten_kappa - wp_w))
     {
         buffer[len - 1]--;
         rest += ten_kappa;
@@ -488,8 +489,8 @@ inline void Prettify(char *buffer, int length, int k)
 inline void dtoa_milo(double value, char *buffer)
 {
     // Not handling NaN and inf
-    assert(!isnan(value));
-    assert(!isinf(value));
+    assert(!std::isnan(value));
+    assert(!std::isinf(value));
 
     if (value == 0)
     {
